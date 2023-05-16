fd0b4b5dae685e3c44275eaec41e0cb8c98db742
Marc Schultz <lunij@users.noreply.github.com>
2023-05-16 14:15:32 +0200

    Remove Quick/Nimble from Typealias tests

---
+++ SourceryTests/Models/TypealiasTests.swift
@@ -0,0 +1,35 @@
+import XCTest
+#if SWIFT_PACKAGE
+@testable import SourceryLib
+#else
+@testable import Sourcery
+#endif
+@testable import SourceryRuntime
+
+class TypealiasTests: XCTestCase {
+    var sut: Typealias!
+
+    override func setUp() {
+        sut = Typealias(aliasName: "Foo", typeName: TypeName(name: "Bar"))
+    }
+
+    func test_reportsName_whenNoParentType() {
+        XCTAssertEqual(sut.name, "Foo")
+    }
+
+    func test_reportsName_whenParentType() {
+        sut?.parent = Type(name: "FooBar", parent: Type(name: "Parent"))
+
+        XCTAssertEqual(sut.name, "Parent.FooBar.Foo")
+    }
+
+    func test_equals_whenSameItems() {
+        XCTAssertEqual(sut, Typealias(aliasName: "Foo", typeName: TypeName(name: "Bar")))
+    }
+
+    func test_diffes_whenDifferentItems() {
+        XCTAssertNotEqual(sut, Typealias(aliasName: "Foo", typeName: TypeName(name: "Foo")))
+        XCTAssertNotEqual(sut, Typealias(aliasName: "Bar", typeName: TypeName(name: "Bar")))
+        XCTAssertNotEqual(sut, Typealias(aliasName: "Bar", typeName: TypeName(name: "Bar"), parent: Type(name: "Parent")))
+    }
+}
