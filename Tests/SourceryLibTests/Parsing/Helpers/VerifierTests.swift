381dcbe171714b37da4a79b2c43fd9e60124a46e
Marc Schultz <lunij@users.noreply.github.com>
2023-05-18 02:48:42 +0200

    Remove Quick/Nimble from Verifier tests

---
+++ Tests/SourceryLibTests/Parsing/Helpers/VerifierTests.swift
@@ -0,0 +1,35 @@
+import SourceryUtils
+import XCTest
+@testable import SourceryFramework
+@testable import SourceryLib
+@testable import SourceryRuntime
+
+class VerifierTests: XCTestCase {
+    func test_allowsEmptyStrings() {
+        XCTAssertEqual(Verifier.canParse(content: "", path: Path("/"), generationMarker: Sourcery.generationMarker), Verifier.Result.approved)
+    }
+
+    func test_rejectsFilesGeneratedBySourcery() {
+        let content = Sourcery.generationMarker + "\n something\n is\n there"
+
+        XCTAssertEqual(Verifier.canParse(content: content, path: Path("/"), generationMarker: Sourcery.generationMarker), Verifier.Result.isCodeGenerated)
+    }
+
+    func test_rejectsFilesGeneratedBySourceryWhenAForceParseExtensionIsDefinedButDoesNotMatchFile() {
+        let content = Sourcery.generationMarker + "\n something\n is\n there"
+
+        XCTAssertEqual(Verifier.canParse(content: content, path: Path("/file.swift"), generationMarker: Sourcery.generationMarker, forceParse: ["toparse"]), Verifier.Result.isCodeGenerated)
+    }
+
+    func test_doesNotRejectFilesGeneratedBySourceryButThatWeWantToForceTheParsingFor() {
+        let content = Sourcery.generationMarker + "\n something\n is\n there"
+
+        XCTAssertEqual(Verifier.canParse(content: content, path: Path("/file.toparse.swift"), generationMarker: Sourcery.generationMarker, forceParse: ["toparse"]), Verifier.Result.approved)
+    }
+
+    func test_rejectsFileContainingConflictMarker() {
+        let content = ["\n<<<<<\n", "\n>>>>>\n"]
+
+        content.forEach { XCTAssertEqual(Verifier.canParse(content: $0, path: Path("/"), generationMarker: Sourcery.generationMarker), Verifier.Result.containsConflictMarkers) }
+    }
+}
