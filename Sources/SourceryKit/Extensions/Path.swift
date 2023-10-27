4d0f93ce5dc3825fe73e1bc649315756190cf3f7
Marc Schultz <lunij@users.noreply.github.com>
2023-10-27 14:05:09 +0200

    Remove FileWritable protocol

--- Sources/SourceryKit/Extensions/Path.swift
+++ Sources/SourceryKit/Extensions/Path.swift
@@ -34,22 +34,7 @@ extension Path {
     func appending(_ path: String) -> Path {
         self + Path(path)
     }
-}
 
-// MARK: - FileWritable
-
-protocol FileWritable {
-    func write(_ string: String, encoding: String.Encoding) throws
-    func writeIfChanged(_ newContent: String) throws
-}
-
-extension FileWritable {
-    func write(_ string: String) throws {
-        try write(string, encoding: .utf8)
-    }
-}
-
-extension Path: FileWritable {
     func writeIfChanged(_ newContent: String) throws {
         guard exists else {
             return try write(newContent)
