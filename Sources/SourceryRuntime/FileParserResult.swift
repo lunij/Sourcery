f21a6e0ae131e7c1318ebc119af2f6473c068dbd
Marc Schultz <lunij@users.noreply.github.com>
2023-11-02 15:14:38 +0100

    Clean up FileParserResult

--- Sources/SourceryRuntime/FileParserResult.swift
+++ Sources/SourceryRuntime/FileParserResult.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-/// :nodoc:
 @objcMembers public final class FileParserResult: NSObject, SourceryModel {
     public let path: String?
     public let module: String?
@@ -12,10 +11,11 @@ import Foundation
             }
         }
     }
-    public var functions = [SourceryMethod]()
-    public var typealiases = [Typealias]()
-    public var inlineRanges = [String: NSRange]()
-    public var inlineIndentations = [String: String]()
+
+    public var functions: [SourceryMethod]
+    public var typealiases: [Typealias]
+    public var inlineRanges: [String: NSRange]
+    public var inlineIndentations: [String: String]
 
     public var modifiedDate: Date
 
@@ -23,7 +23,16 @@ import Foundation
         types.isEmpty && functions.isEmpty && typealiases.isEmpty && inlineRanges.isEmpty && inlineIndentations.isEmpty
     }
 
-    public init(path: String?, module: String?, types: [Type], functions: [SourceryMethod], typealiases: [Typealias] = [], inlineRanges: [String: NSRange] = [:], inlineIndentations: [String: String] = [:], modifiedDate: Date = Date()) {
+    public init(
+        path: String?,
+        module: String?,
+        types: [Type],
+        functions: [SourceryMethod],
+        typealiases: [Typealias] = [],
+        inlineRanges: [String: NSRange] = [:],
+        inlineIndentations: [String: String] = [:],
+        modifiedDate: Date = Date()
+    ) {
         self.path = path
         self.module = module
         self.types = types
@@ -40,30 +49,27 @@ import Foundation
         }
     }
 
-// sourcery:inline:FileParserResult.AutoCoding
+    // sourcery:inline:FileParserResult.AutoCoding
+    public required init?(coder aDecoder: NSCoder) {
+        path = aDecoder.decode(forKey: "path")
+        module = aDecoder.decode(forKey: "module")
+        guard let types: [Type] = aDecoder.decode(forKey: "types") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["types"])); fatalError() }; self.types = types
+        guard let functions: [SourceryMethod] = aDecoder.decode(forKey: "functions") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["functions"])); fatalError() }; self.functions = functions
+        guard let typealiases: [Typealias] = aDecoder.decode(forKey: "typealiases") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["typealiases"])); fatalError() }; self.typealiases = typealiases
+        guard let inlineRanges: [String: NSRange] = aDecoder.decode(forKey: "inlineRanges") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["inlineRanges"])); fatalError() }; self.inlineRanges = inlineRanges
+        guard let inlineIndentations: [String: String] = aDecoder.decode(forKey: "inlineIndentations") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["inlineIndentations"])); fatalError() }; self.inlineIndentations = inlineIndentations
+        guard let modifiedDate: Date = aDecoder.decode(forKey: "modifiedDate") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["modifiedDate"])); fatalError() }; self.modifiedDate = modifiedDate
+    }
 
-/// :nodoc:
-public required init?(coder aDecoder: NSCoder) {
-    path = aDecoder.decode(forKey: "path")
-    module = aDecoder.decode(forKey: "module")
-    guard let types: [Type] = aDecoder.decode(forKey: "types") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["types"])); fatalError() }; self.types = types
-    guard let functions: [SourceryMethod] = aDecoder.decode(forKey: "functions") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["functions"])); fatalError() }; self.functions = functions
-    guard let typealiases: [Typealias] = aDecoder.decode(forKey: "typealiases") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["typealiases"])); fatalError() }; self.typealiases = typealiases
-    guard let inlineRanges: [String: NSRange] = aDecoder.decode(forKey: "inlineRanges") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["inlineRanges"])); fatalError() }; self.inlineRanges = inlineRanges
-    guard let inlineIndentations: [String: String] = aDecoder.decode(forKey: "inlineIndentations") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["inlineIndentations"])); fatalError() }; self.inlineIndentations = inlineIndentations
-    guard let modifiedDate: Date = aDecoder.decode(forKey: "modifiedDate") else { NSException.raise(NSExceptionName.parseErrorException, format: "Key '%@' not found.", arguments: getVaList(["modifiedDate"])); fatalError() }; self.modifiedDate = modifiedDate
-}
-
-/// :nodoc:
-public func encode(with aCoder: NSCoder) {
-    aCoder.encode(path, forKey: "path")
-    aCoder.encode(module, forKey: "module")
-    aCoder.encode(types, forKey: "types")
-    aCoder.encode(functions, forKey: "functions")
-    aCoder.encode(typealiases, forKey: "typealiases")
-    aCoder.encode(inlineRanges, forKey: "inlineRanges")
-    aCoder.encode(inlineIndentations, forKey: "inlineIndentations")
-    aCoder.encode(modifiedDate, forKey: "modifiedDate")
-}
-// sourcery:end
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(path, forKey: "path")
+        aCoder.encode(module, forKey: "module")
+        aCoder.encode(types, forKey: "types")
+        aCoder.encode(functions, forKey: "functions")
+        aCoder.encode(typealiases, forKey: "typealiases")
+        aCoder.encode(inlineRanges, forKey: "inlineRanges")
+        aCoder.encode(inlineIndentations, forKey: "inlineIndentations")
+        aCoder.encode(modifiedDate, forKey: "modifiedDate")
+    }
+    // sourcery:end
 }
