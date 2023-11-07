6dd95821e649991b65be5bcfb7f502715f1f2bde
Marc Schultz <lunij@users.noreply.github.com>
2023-11-07 13:21:28 +0100

    Remove KVC stuff

--- Tests/SourceryKitTests/AST/TypedTests.generated.swift
+++ Tests/SourceryKitTests/AST/TypedTests.generated.swift
@@ -7,230 +7,230 @@ class TypedTests: XCTestCase {
 
     // MARK: - AssociatedValue
 
-    func test_AssociatedValue_canReportOptionalViaKVC() {
-        XCTAssertEqual(AssociatedValue(typeName: .optionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(AssociatedValue(typeName: .optionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, false)
-        XCTAssertEqual(AssociatedValue(typeName: .optionalInt).value(forKeyPath: "unwrappedTypeName") as? String, "Int")
-        XCTAssertEqual(AssociatedValue(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(AssociatedValue(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, true)
+    func test_AssociatedValue_canReportOptional() {
+        XCTAssertEqual(AssociatedValue(typeName: .optionalInt).isOptional, true)
+        XCTAssertEqual(AssociatedValue(typeName: .optionalInt).isImplicitlyUnwrappedOptional, false)
+        XCTAssertEqual(AssociatedValue(typeName: .optionalInt).unwrappedTypeName, "Int")
+        XCTAssertEqual(AssociatedValue(typeName: .implicitlyUnwrappedOptionalInt).isOptional, true)
+        XCTAssertEqual(AssociatedValue(typeName: .implicitlyUnwrappedOptionalInt).isImplicitlyUnwrappedOptional, true)
     }
 
-    func test_AssociatedValue_canReportTupleTypeViaKVC() {
+    func test_AssociatedValue_canReportTupleType() {
         let sut = AssociatedValue(typeName: .doubleIntTuple)
-        XCTAssertEqual(sut.value(forKeyPath: "isTuple") as? Bool, true)
+        XCTAssertEqual(sut.isTuple, true)
     }
 
-    func test_AssociatedValue_canReportClosureTypeViaKVC() {
+    func test_AssociatedValue_canReportClosureType() {
         let sut = AssociatedValue(typeName: .closure)
-        XCTAssertEqual(sut.value(forKeyPath: "isClosure") as? Bool, true)
+        XCTAssertEqual(sut.isClosure, true)
     }
 
-    func test_AssociatedValue_canReportArrayTypeViaKVC() {
+    func test_AssociatedValue_canReportArrayType() {
         let sut = AssociatedValue(typeName: .intArray)
-        XCTAssertEqual(sut.value(forKeyPath: "isArray") as? Bool, true)
+        XCTAssertEqual(sut.isArray, true)
     }
 
-    func test_AssociatedValue_canReportDictionaryTypeViaKVC() {
+    func test_AssociatedValue_canReportDictionaryType() {
         let sut = AssociatedValue(typeName: .intIntDictionary)
-        XCTAssertEqual(sut.value(forKeyPath: "isDictionary") as? Bool, true)
+        XCTAssertEqual(sut.isDictionary, true)
     }
 
-    func test_AssociatedValue_canReportActualTypeNameViaKVC() {
+    func test_AssociatedValue_canReportActualTypeName() {
         let sut = AssociatedValue(typeName: "Alias".typeName)
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Alias".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Alias".typeName)
 
         sut.typeName.actualTypeName = "Int".typeName
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Int".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Int".typeName)
     }
 
     // MARK: - ClosureParameter
 
-    func test_ClosureParameter_canReportOptionalViaKVC() {
-        XCTAssertEqual(ClosureParameter(typeName: .optionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(ClosureParameter(typeName: .optionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, false)
-        XCTAssertEqual(ClosureParameter(typeName: .optionalInt).value(forKeyPath: "unwrappedTypeName") as? String, "Int")
-        XCTAssertEqual(ClosureParameter(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(ClosureParameter(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, true)
+    func test_ClosureParameter_canReportOptional() {
+        XCTAssertEqual(ClosureParameter(typeName: .optionalInt).isOptional, true)
+        XCTAssertEqual(ClosureParameter(typeName: .optionalInt).isImplicitlyUnwrappedOptional, false)
+        XCTAssertEqual(ClosureParameter(typeName: .optionalInt).unwrappedTypeName, "Int")
+        XCTAssertEqual(ClosureParameter(typeName: .implicitlyUnwrappedOptionalInt).isOptional, true)
+        XCTAssertEqual(ClosureParameter(typeName: .implicitlyUnwrappedOptionalInt).isImplicitlyUnwrappedOptional, true)
     }
 
-    func test_ClosureParameter_canReportTupleTypeViaKVC() {
+    func test_ClosureParameter_canReportTupleType() {
         let sut = ClosureParameter(typeName: .doubleIntTuple)
-        XCTAssertEqual(sut.value(forKeyPath: "isTuple") as? Bool, true)
+        XCTAssertEqual(sut.isTuple, true)
     }
 
-    func test_ClosureParameter_canReportClosureTypeViaKVC() {
+    func test_ClosureParameter_canReportClosureType() {
         let sut = ClosureParameter(typeName: .closure)
-        XCTAssertEqual(sut.value(forKeyPath: "isClosure") as? Bool, true)
+        XCTAssertEqual(sut.isClosure, true)
     }
 
-    func test_ClosureParameter_canReportArrayTypeViaKVC() {
+    func test_ClosureParameter_canReportArrayType() {
         let sut = ClosureParameter(typeName: .intArray)
-        XCTAssertEqual(sut.value(forKeyPath: "isArray") as? Bool, true)
+        XCTAssertEqual(sut.isArray, true)
     }
 
-    func test_ClosureParameter_canReportDictionaryTypeViaKVC() {
+    func test_ClosureParameter_canReportDictionaryType() {
         let sut = ClosureParameter(typeName: .intIntDictionary)
-        XCTAssertEqual(sut.value(forKeyPath: "isDictionary") as? Bool, true)
+        XCTAssertEqual(sut.isDictionary, true)
     }
 
-    func test_ClosureParameter_canReportActualTypeNameViaKVC() {
+    func test_ClosureParameter_canReportActualTypeName() {
         let sut = ClosureParameter(typeName: "Alias".typeName)
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Alias".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Alias".typeName)
 
         sut.typeName.actualTypeName = "Int".typeName
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Int".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Int".typeName)
     }
 
     // MARK: - MethodParameter
 
-    func test_MethodParameter_canReportOptionalViaKVC() {
-        XCTAssertEqual(MethodParameter(typeName: .optionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(MethodParameter(typeName: .optionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, false)
-        XCTAssertEqual(MethodParameter(typeName: .optionalInt).value(forKeyPath: "unwrappedTypeName") as? String, "Int")
-        XCTAssertEqual(MethodParameter(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(MethodParameter(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, true)
+    func test_MethodParameter_canReportOptional() {
+        XCTAssertEqual(MethodParameter(typeName: .optionalInt).isOptional, true)
+        XCTAssertEqual(MethodParameter(typeName: .optionalInt).isImplicitlyUnwrappedOptional, false)
+        XCTAssertEqual(MethodParameter(typeName: .optionalInt).unwrappedTypeName, "Int")
+        XCTAssertEqual(MethodParameter(typeName: .implicitlyUnwrappedOptionalInt).isOptional, true)
+        XCTAssertEqual(MethodParameter(typeName: .implicitlyUnwrappedOptionalInt).isImplicitlyUnwrappedOptional, true)
     }
 
-    func test_MethodParameter_canReportTupleTypeViaKVC() {
+    func test_MethodParameter_canReportTupleType() {
         let sut = MethodParameter(typeName: .doubleIntTuple)
-        XCTAssertEqual(sut.value(forKeyPath: "isTuple") as? Bool, true)
+        XCTAssertEqual(sut.isTuple, true)
     }
 
-    func test_MethodParameter_canReportClosureTypeViaKVC() {
+    func test_MethodParameter_canReportClosureType() {
         let sut = MethodParameter(typeName: .closure)
-        XCTAssertEqual(sut.value(forKeyPath: "isClosure") as? Bool, true)
+        XCTAssertEqual(sut.isClosure, true)
     }
 
-    func test_MethodParameter_canReportArrayTypeViaKVC() {
+    func test_MethodParameter_canReportArrayType() {
         let sut = MethodParameter(typeName: .intArray)
-        XCTAssertEqual(sut.value(forKeyPath: "isArray") as? Bool, true)
+        XCTAssertEqual(sut.isArray, true)
     }
 
-    func test_MethodParameter_canReportDictionaryTypeViaKVC() {
+    func test_MethodParameter_canReportDictionaryType() {
         let sut = MethodParameter(typeName: .intIntDictionary)
-        XCTAssertEqual(sut.value(forKeyPath: "isDictionary") as? Bool, true)
+        XCTAssertEqual(sut.isDictionary, true)
     }
 
-    func test_MethodParameter_canReportActualTypeNameViaKVC() {
+    func test_MethodParameter_canReportActualTypeName() {
         let sut = MethodParameter(typeName: "Alias".typeName)
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Alias".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Alias".typeName)
 
         sut.typeName.actualTypeName = "Int".typeName
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Int".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Int".typeName)
     }
 
     // MARK: - TupleElement
 
-    func test_TupleElement_canReportOptionalViaKVC() {
-        XCTAssertEqual(TupleElement(typeName: .optionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(TupleElement(typeName: .optionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, false)
-        XCTAssertEqual(TupleElement(typeName: .optionalInt).value(forKeyPath: "unwrappedTypeName") as? String, "Int")
-        XCTAssertEqual(TupleElement(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(TupleElement(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, true)
+    func test_TupleElement_canReportOptional() {
+        XCTAssertEqual(TupleElement(typeName: .optionalInt).isOptional, true)
+        XCTAssertEqual(TupleElement(typeName: .optionalInt).isImplicitlyUnwrappedOptional, false)
+        XCTAssertEqual(TupleElement(typeName: .optionalInt).unwrappedTypeName, "Int")
+        XCTAssertEqual(TupleElement(typeName: .implicitlyUnwrappedOptionalInt).isOptional, true)
+        XCTAssertEqual(TupleElement(typeName: .implicitlyUnwrappedOptionalInt).isImplicitlyUnwrappedOptional, true)
     }
 
-    func test_TupleElement_canReportTupleTypeViaKVC() {
+    func test_TupleElement_canReportTupleType() {
         let sut = TupleElement(typeName: .doubleIntTuple)
-        XCTAssertEqual(sut.value(forKeyPath: "isTuple") as? Bool, true)
+        XCTAssertEqual(sut.isTuple, true)
     }
 
-    func test_TupleElement_canReportClosureTypeViaKVC() {
+    func test_TupleElement_canReportClosureType() {
         let sut = TupleElement(typeName: .closure)
-        XCTAssertEqual(sut.value(forKeyPath: "isClosure") as? Bool, true)
+        XCTAssertEqual(sut.isClosure, true)
     }
 
-    func test_TupleElement_canReportArrayTypeViaKVC() {
+    func test_TupleElement_canReportArrayType() {
         let sut = TupleElement(typeName: .intArray)
-        XCTAssertEqual(sut.value(forKeyPath: "isArray") as? Bool, true)
+        XCTAssertEqual(sut.isArray, true)
     }
 
-    func test_TupleElement_canReportDictionaryTypeViaKVC() {
+    func test_TupleElement_canReportDictionaryType() {
         let sut = TupleElement(typeName: .intIntDictionary)
-        XCTAssertEqual(sut.value(forKeyPath: "isDictionary") as? Bool, true)
+        XCTAssertEqual(sut.isDictionary, true)
     }
 
-    func test_TupleElement_canReportActualTypeNameViaKVC() {
+    func test_TupleElement_canReportActualTypeName() {
         let sut = TupleElement(typeName: "Alias".typeName)
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Alias".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Alias".typeName)
 
         sut.typeName.actualTypeName = "Int".typeName
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Int".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Int".typeName)
     }
 
     // MARK: - Typealias
 
-    func test_Typealias_canReportOptionalViaKVC() {
-        XCTAssertEqual(Typealias(typeName: .optionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(Typealias(typeName: .optionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, false)
-        XCTAssertEqual(Typealias(typeName: .optionalInt).value(forKeyPath: "unwrappedTypeName") as? String, "Int")
-        XCTAssertEqual(Typealias(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(Typealias(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, true)
+    func test_Typealias_canReportOptional() {
+        XCTAssertEqual(Typealias(typeName: .optionalInt).isOptional, true)
+        XCTAssertEqual(Typealias(typeName: .optionalInt).isImplicitlyUnwrappedOptional, false)
+        XCTAssertEqual(Typealias(typeName: .optionalInt).unwrappedTypeName, "Int")
+        XCTAssertEqual(Typealias(typeName: .implicitlyUnwrappedOptionalInt).isOptional, true)
+        XCTAssertEqual(Typealias(typeName: .implicitlyUnwrappedOptionalInt).isImplicitlyUnwrappedOptional, true)
     }
 
-    func test_Typealias_canReportTupleTypeViaKVC() {
+    func test_Typealias_canReportTupleType() {
         let sut = Typealias(typeName: .doubleIntTuple)
-        XCTAssertEqual(sut.value(forKeyPath: "isTuple") as? Bool, true)
+        XCTAssertEqual(sut.isTuple, true)
     }
 
-    func test_Typealias_canReportClosureTypeViaKVC() {
+    func test_Typealias_canReportClosureType() {
         let sut = Typealias(typeName: .closure)
-        XCTAssertEqual(sut.value(forKeyPath: "isClosure") as? Bool, true)
+        XCTAssertEqual(sut.isClosure, true)
     }
 
-    func test_Typealias_canReportArrayTypeViaKVC() {
+    func test_Typealias_canReportArrayType() {
         let sut = Typealias(typeName: .intArray)
-        XCTAssertEqual(sut.value(forKeyPath: "isArray") as? Bool, true)
+        XCTAssertEqual(sut.isArray, true)
     }
 
-    func test_Typealias_canReportDictionaryTypeViaKVC() {
+    func test_Typealias_canReportDictionaryType() {
         let sut = Typealias(typeName: .intIntDictionary)
-        XCTAssertEqual(sut.value(forKeyPath: "isDictionary") as? Bool, true)
+        XCTAssertEqual(sut.isDictionary, true)
     }
 
-    func test_Typealias_canReportActualTypeNameViaKVC() {
+    func test_Typealias_canReportActualTypeName() {
         let sut = Typealias(typeName: "Alias".typeName)
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Alias".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Alias".typeName)
 
         sut.typeName.actualTypeName = "Int".typeName
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Int".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Int".typeName)
     }
 
     // MARK: - Variable
 
-    func test_Variable_canReportOptionalViaKVC() {
-        XCTAssertEqual(Variable(typeName: .optionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(Variable(typeName: .optionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, false)
-        XCTAssertEqual(Variable(typeName: .optionalInt).value(forKeyPath: "unwrappedTypeName") as? String, "Int")
-        XCTAssertEqual(Variable(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isOptional") as? Bool, true)
-        XCTAssertEqual(Variable(typeName: .implicitlyUnwrappedOptionalInt).value(forKeyPath: "isImplicitlyUnwrappedOptional") as? Bool, true)
+    func test_Variable_canReportOptional() {
+        XCTAssertEqual(Variable(typeName: .optionalInt).isOptional, true)
+        XCTAssertEqual(Variable(typeName: .optionalInt).isImplicitlyUnwrappedOptional, false)
+        XCTAssertEqual(Variable(typeName: .optionalInt).unwrappedTypeName, "Int")
+        XCTAssertEqual(Variable(typeName: .implicitlyUnwrappedOptionalInt).isOptional, true)
+        XCTAssertEqual(Variable(typeName: .implicitlyUnwrappedOptionalInt).isImplicitlyUnwrappedOptional, true)
     }
 
-    func test_Variable_canReportTupleTypeViaKVC() {
+    func test_Variable_canReportTupleType() {
         let sut = Variable(typeName: .doubleIntTuple)
-        XCTAssertEqual(sut.value(forKeyPath: "isTuple") as? Bool, true)
+        XCTAssertEqual(sut.isTuple, true)
     }
 
-    func test_Variable_canReportClosureTypeViaKVC() {
+    func test_Variable_canReportClosureType() {
         let sut = Variable(typeName: .closure)
-        XCTAssertEqual(sut.value(forKeyPath: "isClosure") as? Bool, true)
+        XCTAssertEqual(sut.isClosure, true)
     }
 
-    func test_Variable_canReportArrayTypeViaKVC() {
+    func test_Variable_canReportArrayType() {
         let sut = Variable(typeName: .intArray)
-        XCTAssertEqual(sut.value(forKeyPath: "isArray") as? Bool, true)
+        XCTAssertEqual(sut.isArray, true)
     }
 
-    func test_Variable_canReportDictionaryTypeViaKVC() {
+    func test_Variable_canReportDictionaryType() {
         let sut = Variable(typeName: .intIntDictionary)
-        XCTAssertEqual(sut.value(forKeyPath: "isDictionary") as? Bool, true)
+        XCTAssertEqual(sut.isDictionary, true)
     }
 
-    func test_Variable_canReportActualTypeNameViaKVC() {
+    func test_Variable_canReportActualTypeName() {
         let sut = Variable(typeName: "Alias".typeName)
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Alias".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Alias".typeName)
 
         sut.typeName.actualTypeName = "Int".typeName
-        XCTAssertEqual(sut.value(forKeyPath: "actualTypeName") as? TypeName, "Int".typeName)
+        XCTAssertEqual(sut.actualTypeName, "Int".typeName)
     }
 }
 
